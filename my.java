b178bfe04e35b0424eb51b30b4d69d3646da01af
krissloveis <yorkyy@mail.ru>
2025-07-02 22:19:13 +0300

    1

--- my.java
+++ my.java
@@ -1,10 +1,25 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class my {
     public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now.format(formatter));
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+//        LocalDateTime now = LocalDateTime.now();
+//        System.out.println(now.format(formatter));
+
+        while (true) {
+            System.out.println("Введите сообщение:");
+            String input = new Scanner(System.in).nextLine();
+             switch (input) {
+                 case "Hello!" -> System.out.println("Hello!)");
+                 case "How are you?" -> {
+                     System.out.println("I'm Fine!");
+                     System.out.println("And you?");
+                 }
+                 default -> System.out.println("I don't understand :(");
+
+             }
+        }
     }
 }
